d394e0e8f049daad23d6a35e2b5d27ae66e1244a
PozhilayaKrevetka192 <7545245@ukr.net>
2023-09-18 19:38:18 +0300

    s

--- js/modules/loto-navigation.js
+++ js/modules/loto-navigation.js
@@ -27,8 +27,8 @@ let activeFinishTimers = {
 };
 
 export const connectWebsocketFunctions = () => {
-  // const ws = new WebSocket(`ws://localhost:5001/game`);
-  const ws = new WebSocket(`wss://loto-server-new.onrender.com/game`);
+  const ws = new WebSocket(`ws://localhost:5001/game`);
+  // const ws = new WebSocket(`wss://loto-server-new.onrender.com/game`);
   window.ws = ws;
   let clientId = createClientId();
 
@@ -86,9 +86,10 @@ export const connectWebsocketFunctions = () => {
         await startMenuTimerLobby(msg.timers);
 
         for (let room = 1; room <= 5; room++) {
-          if (activeFinishTimers[`room${room}`] != null) {
-            clearInterval(activeFinishTimers[`room${room}`]);
-            activeFinishTimers[`room${room}`] = null;
+          let roomTimer = activeFinishTimers[`room${room}`];
+          if (roomTimer != null) {
+            roomTimer = null;
+            clearInterval(roomTimer);
           }
         }
 
@@ -107,12 +108,11 @@ export const connectWebsocketFunctions = () => {
         updateAllRoomsJackpot(msg.jackpots);
         break;
       case "allRoomsStartTimers":
-        // console.log(msg);
-        // console.log(activeTimers);
         for (let room = 1; room <= 5; room++) {
-          if (activeTimers[`room${room}`] != null) {
-            clearInterval(activeTimers[`room${room}`]);
-            activeTimers[`room${room}`] = null;
+          let roomTimer = activeTimers[`room${room}`];
+          if (roomTimer != null) {
+            roomTimer = null;
+            clearInterval(roomTimer);
           }
         }
         // for (let timer of activeTimers) {
@@ -122,12 +122,12 @@ export const connectWebsocketFunctions = () => {
 
         break;
       case "allRoomsFinishTimers":
-        console.log(activeFinishTimers);
-        console.log(msg);
+        // console.log(msg);
         for (let room = 1; room <= 5; room++) {
-          if (activeFinishTimers[`room${room}`] != null) {
-            clearInterval(activeFinishTimers[`room${room}`]);
-            activeFinishTimers[`room${room}`] = null;
+          let roomTimer = activeFinishTimers[`room${room}`];
+          if (roomTimer != null) {
+            roomTimer = null;
+            clearInterval(roomTimer);
           }
         }
         // for (let timer of activeFinishTimers) {
@@ -613,8 +613,8 @@ async function NowClientTime() {
   // console.log("now time", new Date().getTime());
   // console.log("api time", time);
 
-  // return timeHands - 180 * 60 * 1000;
-  return timeHands;
+  return timeHands - 180 * 60 * 1000;
+  // return timeHands;
 }
 
 // "year": "2023",
@@ -954,7 +954,6 @@ async function startMenuTimerLobby(timers) {
             }
           }
         }, 1000);
-        clearInterval(activeTimers[`room${roomId}`]);
         activeTimers[`room${roomId}`] = timer;
       }
     }
@@ -1028,7 +1027,6 @@ async function startMenuTimerGame(timers) {
           }
         }, 1000);
 
-        clearInterval(activeFinishTimers[`room${room}`]);
         activeFinishTimers[`room${roomId}`] = timer;
       }
     }
